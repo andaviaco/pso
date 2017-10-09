794fc978aed6a169e392f28388565ea79034a72f
Andrés Ávila <andaviaco@gmail.com>
2017-10-09 03:00:07 -0500

    add sphere, ackley and rastrigin functions

--- src/lib.py
+++ src/lib.py
@@ -5,8 +5,24 @@ import operator
 from functools import reduce
 
 
-def griewank(*args):
-    sumatory = 1.0 / 4000 * np.sum([(x**2) for x in args])
-    product = np.prod([np.cos(x / np.sqrt(i+1.0)) for i, x in enumerate(args)])
+def griewank(d):
+    sumatory = 1.0 / 4000 * np.sum([(x**2) for x in d])
+    product = np.prod([np.cos(x / np.sqrt(i+1.0)) for i, x in enumerate(d)])
 
     return 1 + sumatory - product
+
+def sphere(d):
+    return np.sum([x**2 for x in d])
+
+def ackley(d, *, a=20, b=0.2, c=2*np.pi):
+    sum_part1 = np.sum([x**2 for x in d])
+    part1 = -1.0 * a * np.exp(-1.0 * b * np.sqrt((1.0/len(d)) * sum_part1))
+
+    sum_part2 = np.sum([np.cos(c * x) for x in d])
+    part2 = -1.0 * np.exp((1.0 / len(d)) * sum_part2)
+
+    return a + np.exp(1) + part1 + part2
+
+def rastrigin(d):
+    sum_i = np.sum([x**2 - 10*np.cos(2 * np.pi * x) for x in d])
+    return 10 * len(d) + sum_i
--- src/main.py
+++ src/main.py
@@ -4,10 +4,18 @@ from swarm import Swarm
 
 
 def main():
-    swarm = Swarm(10, 10, lib.griewank)
+    swarm_sphere = Swarm(10, 10, lib.sphere, fn_lb=[-5, -5], fn_ub=[5, 5])
+    swarm_ackley = Swarm(10, 10, lib.ackley, fn_lb=[-20, -20], fn_ub=[20, 20])
+    swarm_rastrigin = Swarm(10, 10, lib.rastrigin, fn_lb=[-5, -5], fn_ub=[5, 5])
 
-    min_x, min_y = swarm.optimize()
-    print(f'Mínimo: x={min_x}, y={min_y}')
+    min_x, min_y = swarm_sphere.optimize()
+    print(f'Sphere min: x={min_x}, y={min_y}')
+
+    min_x, min_y = swarm_ackley.optimize()
+    print(f'Ackley min: x={min_x}, y={min_y}')
+
+    min_x, min_y = swarm_rastrigin.optimize()
+    print(f'Rastrigin min: x={min_x}, y={min_y}')
 
 if __name__ == '__main__':
     main()
